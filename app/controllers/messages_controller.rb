4c20f3c3faab6f3b13ff03734da0d99637a2812a
pauline rouger <paulinerouge@hotmail.com>
2021-12-13 22:18:47 +0800

    Broadcast new message

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -5,6 +5,11 @@ class MessagesController < ApplicationController
     @message.chatroom = @chatroom
     @message.user = current_user
     if @message.save
+      # send to the channel
+      ChatroomChannel.broadcast_to(
+      @chatroom,
+      render_to_string(partial: "message", locals: { message: @message })
+      )
       redirect_to chatroom_path(@chatroom, anchor: "message-#{@message.id}")
     else
       render "chatrooms/show"
